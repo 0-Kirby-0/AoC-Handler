1bcaa05940910a7299601d33d0aa5500fea630b4
0-Kirby-0 <adlerkorbinian98@gmail.com>
2025-12-08 02:41:32 +0100

    Improved cache system and token handling

--- src/input_handler/cache.rs
+++ src/input_handler/cache.rs
@@ -1,29 +1,62 @@
-use std::{
-    fs::{self, File},
-    io,
-    path::PathBuf,
-};
+use core::panic;
+use std::path::{Path, PathBuf};
 
-pub fn is_cached(sub_path: &PathBuf) -> bool {
-    file_dir(sub_path).exists()
+pub fn is_cached(sub_path: &Path) -> bool {
+    file_path(sub_path).exists()
 }
 
-pub fn cache<'a>(sub_path: &PathBuf, data: impl Into<&'a [u8]>) -> io::Result<()> {
-    std::fs::write(file_dir(sub_path), data.into())
+pub fn cache(sub_path: &Path, data: impl AsRef<[u8]>) -> std::io::Result<()> {
+    std::fs::write(file_path(sub_path), data)
 }
 
-pub fn get_cached(sub_path: &PathBuf) -> File {
-    std::fs::File::open(file_dir(sub_path)).expect("Unable to retrieve cached file.")
+pub fn get_cached(sub_path: &Path) -> std::io::Result<std::fs::File> {
+    std::fs::File::open(file_path(sub_path))
 }
 
-fn file_dir(sub_path: &PathBuf) -> PathBuf {
-    let path = cache_dir().join(sub_path);
-    fs::create_dir_all(path.parent().unwrap()).expect("Unable to create requested cache dirs.");
+pub fn clear_cached(sub_path: &Path) -> std::io::Result<()> {
+    std::fs::remove_file(file_path(sub_path))
+}
+
+fn file_path(sub_path: &Path) -> PathBuf {
+    let path = dirs::cache_dir()
+        .expect("dirs did not provide a cache location.")
+        .join("aoc_handler") //Parent folder for this cache
+        .join(sub_path);
+    std::fs::create_dir_all(path.parent().unwrap()).unwrap_or_else(|_| {
+        panic!(
+            "Unable to create requested cache directory '{}'",
+            path.display()
+        )
+    });
+
     path
 }
 
-fn cache_dir() -> PathBuf {
-    dirs::cache_dir()
-        .expect("Could not find cache location.")
-        .join("aoc_input")
+pub mod text {
+    use anyhow::Context;
+    use std::io::Read;
+    use std::path::Path;
+
+    pub fn cache_text(sub_path: &Path, text: &str) -> std::io::Result<()> {
+        if text.ends_with('\n') {
+            super::cache(sub_path, text)
+        } else {
+            let mut text = text.to_owned();
+            text.push('\n');
+            super::cache(sub_path, text)
+        }
+    }
+
+    pub fn get_cached_text(sub_path: &Path) -> anyhow::Result<String> {
+        let mut text = String::default();
+        super::get_cached(sub_path)?
+            .read_to_string(&mut text)
+            .with_context(|| {
+                format!(
+                    "Unable to read cached file at '{}' to string.",
+                    sub_path.display()
+                )
+            })?;
+        Ok(text.trim().to_owned())
+    }
 }
--- src/input_handler/mod.rs
+++ src/input_handler/mod.rs
@@ -1,87 +1,132 @@
-#![allow(dead_code, unused_variables)]
-
-use std::{
-    io::{self, Read},
-    path::PathBuf,
-};
-
-use downloader::Downloader;
-
 use crate::{Day, Year};
 
 mod cache;
-mod downloader;
 
 pub struct InputHandler {
-    year: Year,
-    downloader: Downloader,
+    client: reqwest::blocking::Client,
+    token: Token,
 }
 
 impl InputHandler {
-    pub fn new(year: Year) -> Self {
-        InputHandler {
-            year,
-            downloader: Downloader::new(Self::get_token()),
+    pub fn new() -> Self {
+        Self {
+            client: reqwest::blocking::Client::new(),
+            token: Token::new(),
         }
     }
-    pub fn get_day_input(&self, day: Day) -> String {
-        let sub_path = format!("{}/day{}_input.txt", self.year, day).into();
 
-        if !cache::is_cached(&sub_path) {
-            let loaded = self.downloader.get(self.year, day);
-            cache::cache(&sub_path, loaded.as_bytes()).expect("Unable to cache input.");
-        }
+    /// Fetches the input data for a given day and year of Advent of Code.
+    /// Preferentially sources from internal cache.
+    pub fn get_day_input(&self, year: Year, day: Day) -> String {
+        let sub_path: std::path::PathBuf = format!("{year}/day{day}_input.txt").into();
 
-        let mut out_string = String::default();
-        cache::get_cached(&sub_path)
-            .read_to_string(&mut out_string)
-            .expect("Unable to read cached input to string.");
-        out_string
-    }
-
-    fn get_token() -> String {
-        let sub_path: PathBuf = "token.txt".into();
-        let mut token: String = Default::default();
-
+        //If the file is already cached, we trust that it's fine, because we wouldn't cache a broken file.
+        //(And if we did, the user can delete the cache themselves)
         if cache::is_cached(&sub_path) {
-            cache::get_cached(&sub_path)
-                .read_to_string(&mut token)
-                .expect("Unable to read token.");
-            return token;
+            return cache::text::get_cached_text(&sub_path)
+                .expect("Couldn't get cached input file, even though it exists.");
         }
 
+        let url = format!("https://adventofcode.com/{year}/day/{day}/input");
+
+        let input = self
+            .client
+            .get(url)
+            .header("Cookie", format!("session={}", self.token))
+            .send()
+            .expect("Failed to send request to AoC servers. Please check your internet connection.")
+            .text()
+            .expect("Failed to get response text from AoC servers. Please check if their servers are offline and try again later.")
+            .trim()
+            .to_owned();
+
+        if input == "Puzzle inputs differ by user.  Please log in to get your puzzle input." {
+            self.token.invalidate();
+            panic!("Cached Advent of Code session token failed to validate. Please try again to be prompted for the updated token.");
+            //? I cannot be bothered to build a loop that appropriately connects back to this same point.
+            //? This harness is called repeatedly on solution re-compiles anyway, one more is fine.
+        }
+
+        cache::text::cache_text(&sub_path, &input).expect("Unable to cache input file.");
+        input
+    }
+}
+
+struct Token {
+    token: String,
+    path: std::path::PathBuf,
+}
+
+/*
+A note on security:
+Storing a token in plain text in file, no encryption, not even perm restrictions, feels horrible.
+However we have to acknowledge that this token is for AoC, a toy web page. There are no consequences of a token leak.
+Any security overhead we accept here would just be posturing, it's not worth it. */
+
+impl Token {
+    pub fn new() -> Self {
+        let path: std::path::PathBuf = "token.txt".into();
+
+        Self {
+            token: if cache::is_cached(&path) {
+                Self::get_token_from_cache(&path)
+            } else {
+                let token = Self::get_token_from_user();
+                Self::cache_token(&path, &token);
+                token
+            },
+            path,
+        }
+    }
+    pub fn invalidate(&self) {
+        cache::clear_cached(&self.path).expect("Unable to clear cached token file.");
+    }
+
+    fn get_token_from_cache(path: &std::path::Path) -> String {
+        cache::text::get_cached_text(path).expect("Unable to get cached token.")
+    }
+    fn cache_token(path: &std::path::Path, token: &str) {
+        cache::text::cache_text(path, token)
+            .unwrap_or_else(|e| eprintln!("Unable to cache given token: {e}"));
+    }
+
+    fn get_token_from_user() -> String {
         println!("Please provide your AoC access token:");
         println!("- 128 character hexadecimal string");
         println!("- Found by inspecting AoC page requests in your browser");
         println!(
             "- Will be cached unencrypted in: {}",
             dirs::cache_dir()
-                .map(|p| p.display().to_string())
-                .unwrap_or_else(|| "cache directory".to_string())
-        );
-        println!(
-            "Should there be issues with the currently cached token, manually delete it there."
+                .expect("dirs did not provide a cache directory.")
+                .display()
         );
 
-        io::stdin()
-            .read_to_string(&mut token)
+        let mut maybe_token = String::default();
+        std::io::stdin()
+            .read_line(&mut maybe_token)
             .expect("Unable to read given token.");
+        let maybe_token = maybe_token.trim().to_string();
+        Self::validate_format(&maybe_token).expect("The provided token was invalid:");
 
-        token = token.trim().to_lowercase().to_owned(); //cleaning up, after all we're nice :)
-
-        Self::validate_token(&token).unwrap();
-
-        cache::cache(&sub_path, token.as_bytes()).expect("Unable to cache token.");
-        token
+        maybe_token
     }
 
-    fn validate_token(token: &str) -> Result<(), String> {
-        if token.len() != 128 {
+    fn validate_format(maybe_token: &str) -> Result<(), String> {
+        if maybe_token.len() != 128 {
             Err("Token is incorrect length. Should be 128 characters.".to_string())
-        } else if !(token.chars().all(|c| matches!(c, '0'..='9' | 'a'..='f'))) {
+        } else if !(maybe_token
+            .chars()
+            .all(|c| matches!(c, '0'..='9' | 'a'..='f')))
+        {
             Err("Token contains invalid characters. Must be hexadecimal.".to_string())
         } else {
             Ok(())
         }
+    }
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.token)
     }
 }
